2ce096a3e9fdae2858e8ed2a824251c92c468a30
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-07-10 19:29:31 +0900

    update function

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log(handTrack);
+// console.log(handTrack);
+
+const video = document.getElementById("myvideo");
+const canvas = document.getElementById("mycanvas");
+
+let model;
+const options = {
+    // flipHorizonal: false,
+    maxNumBoxes: 3,
+    scoreThreshold: 0.7,
+}
+
+let context = canvas.getContext("2d");
+
+
+handTrack.load(options).then(function(modelData){
+    model = modelData;
+    console.log(model);
+
+    handTrack.startVideo(video).then(function(status){
+        if(status) {
+            console.log(status);
+            startDetection();
+        } else {
+            console.log("failed");
+        }
+    })
+})
+
+function startDetection() {
+    model.detect(video).then((predictions) => {
+        model.renderPredictions(predictions, canvas, context, video);
+
+        requestAnimationFrame(startDetection);
+    });
+}
